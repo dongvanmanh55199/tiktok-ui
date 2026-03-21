6ac7554a22d4cd0ca49221ccade374506b74ae2b
dongvanmanh55199 <127465983+dongvanmanh55199@users.noreply.github.com>
2026-03-21 22:01:55 +0900

    Update httpRequest.js
    Update API base URL

--- src/ultils/httpRequest.js
+++ src/ultils/httpRequest.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 console.log(process.env.REACT_APP_BASE_URL)
 
 const httpRequest = axios.create({
-   baseURL: 'https://tiktok.fullstack.edu.vn/api/',
+   baseURL: 'https://tiktok.f8.edu.vn/api/',
 })
 
 export const get = async (path, option = {}) => {
